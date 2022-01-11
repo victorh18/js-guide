101151862ffc5d88a54ab0a986fc0e51a04e6dea
Victor de la Rosa <videlarosa@humano.com.do>
2022-01-11 11:35:28 -0400

    More progress...

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -2,12 +2,17 @@ window.onload = () => {
     const defaultResult = 0;
     let currentResult = defaultResult;
 
+    function getUserInput() {
+        return parseInt(userInput.value);
+    }
+
     function add() {
-        currentResult += userInput.value;
-        outputResult(currentResult, '');
+        const initialValue = currentResult;
+        const enteredNumber = getUserInput();
+        currentResult += enteredNumber;
+        outputResult(currentResult, `${initialValue} + ${enteredNumber}`);
     }
 
     addBtn.addEventListener('click', add);
-
     
 }
