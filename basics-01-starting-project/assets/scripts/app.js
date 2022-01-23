66d82a611b141defb39222c732c22075f660045a
Victor de la Rosa <victorwmh18@gmail.com>
2022-01-23 17:46:48 -0400

    Adding gitignore for vscode configurations

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,68 +1,63 @@
 // window.onload = () => {
-    const defaultResult = 0;
-    let currentResult = defaultResult;
-    let logEntries = [];
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
 
-    function getUserInput() {
-        return parseInt(userInput.value);
-    }
+function getUserInput() {
+  return parseInt(userInput.value);
+}
 
-    function createAndWriteOutput(operator, initialValue, enteredNumber) {
-        const descResult = `${initialValue} ${operator} ${enteredNumber}`;
-        outputResult(currentResult, descResult);
-    }
+function createAndWriteOutput(operator, initialValue, enteredNumber) {
+  const descResult = `${initialValue} ${operator} ${enteredNumber}`;
+  outputResult(currentResult, descResult);
+}
 
-    function writeToLog(
-        operation, 
-        initialNumber, 
-        enteredNumber, 
-        result
-    ) {
-        let entry = {
-            operation, 
-            initialNumber, 
-            enteredNumber, 
-            result
-        };
-        logEntries.push(entry);
-        console.log(logEntries);
-    }
+function writeToLog(operation, initialNumber, enteredNumber, result) {
+  let entry = {
+    operation,
+    initialNumber,
+    enteredNumber,
+    result,
+  };
+  logEntries.push(entry);
+  console.log(logEntries);
+}
 
-    function add() {
-        const enteredNumber = getUserInput();
-        const initialValue = currentResult;
-        currentResult += enteredNumber;
-        createAndWriteOutput('+', initialValue, enteredNumber);
-        writeToLog('ADD', initialValue, enteredNumber, currentResult);
-    }
+function add() {
+  const enteredNumber = getUserInput();
+  const initialValue = currentResult;
+  currentResult += enteredNumber;
+  createAndWriteOutput("+", initialValue, enteredNumber);
+  writeToLog("ADD", initialValue, enteredNumber, currentResult);
+}
 
-    function substract() {
-        const enteredNumber = getUserInput();
-        const initialValue = currentResult;
-        currentResult -= enteredNumber;
-        createAndWriteOutput('-', initialValue, enteredNumber);
-        writeToLog('SUBSTRACT', initialValue, enteredNumber, currentResult);
-        let num = 0
-    }
+function substract() {
+  const enteredNumber = getUserInput();
+  const initialValue = currentResult;
+  currentResult -= enteredNumber;
+  createAndWriteOutput("-", initialValue, enteredNumber);
+  writeToLog("SUBSTRACT", initialValue, enteredNumber, currentResult);
+  let num = 0;
+}
 
-    function multiply() {
-        const enteredNumber = getUserInput();
-        const initialValue = currentResult;
-        currentResult *= enteredNumber;
-        createAndWriteOutput('*', initialValue, enteredNumber);
-        writeToLog('MULTIPLY', initialValue, enteredNumber, currentResult);
-    }
+function multiply() {
+  const enteredNumber = getUserInput();
+  const initialValue = currentResult;
+  currentResult *= enteredNumber;
+  createAndWriteOutput("*", initialValue, enteredNumber);
+  writeToLog("MULTIPLY", initialValue, enteredNumber, currentResult);
+}
 
-    function divide() {
-        const enteredNumber = getUserInput();
-        const initialValue = currentResult;
-        currentResult /= enteredNumber;
-        createAndWriteOutput('/', initialValue, enteredNumber);
-        writeToLog('DIVIDE', initialValue, enteredNumber, currentResult);
-    }
+function divide() {
+  const enteredNumber = getUserInput();
+  const initialValue = currentResult;
+  currentResult /= enteredNumber;
+  createAndWriteOutput("/", initialValue, enteredNumber);
+  writeToLog("DIVIDE", initialValue, enteredNumber, currentResult);
+}
 
-    addBtn.addEventListener('click', add);
-    subtractBtn.addEventListener('click', substract);
-    multiplyBtn.addEventListener('click', multiply);
-    divideBtn.addEventListener('click', divide);
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", substract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
 // }
