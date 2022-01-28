1e8d55617eaa5738f35a91168c3bab9e13dcb4a4
Victor de la Rosa <victorwmh18@gmail.com>
2022-01-27 22:23:09 -0400

    added healing logic

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,6 +1,8 @@
 const ATTACK_VALUE = 20;
 const STRONG_ATTACK_VALUE = 25
 const MONSTER_ATTACK_VALUE = 17
+const HEAL_VALUE = 20;
+
 const AttackModes = {
     NORMAL: 'NORMAL',
     STRONG: 'STRONG'
@@ -10,6 +12,19 @@ let chosenMaxLife = 100;
 
 let playersHealth = chosenMaxLife;
 let monstersHealth = chosenMaxLife;
+
+function endRound() {
+    const monsterDealtDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    playersHealth -= monsterDealtDamage;
+
+    if (playersHealth > 0 && monstersHealth <= 0 ) {
+        alert("you won! :D");
+    } else if (monstersHealth > 0 && playersHealth <= 0) {
+        alert("You lost! :(");
+    } else if (playersHealth <= 0 && monstersHealth <= 0) {
+        alert("It's a draw! .__.");
+    }
+}
 
 function attackMonster(attackMode) {
     let maxDamage;
@@ -23,16 +38,22 @@ function attackMonster(attackMode) {
 
     const dealtDamage = dealMonsterDamage(maxDamage);
     monstersHealth -= dealtDamage;
-    const monsterDealtDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-    playersHealth -= monsterDealtDamage;
 
-    if (playersHealth > 0 && monstersHealth <= 0 ) {
-        alert("you won! :D");
-    } else if (monstersHealth > 0 && playersHealth <= 0) {
-        alert("You lost! :(");
-    } else if (playersHealth <= 0 && monstersHealth <= 0) {
-        alert("It's a draw! .__.");
+    endRound()
+}
+
+function healPlayerHandler() {
+    let healValue;
+    if (playersHealth >= chosenMaxLife - HEAL_VALUE) {
+        healValue = chosenMaxLife - playersHealth;
+    } else {
+        healValue = HEAL_VALUE;
     }
+
+    increasePlayerHealth(healValue);
+    playersHealth += healValue;
+
+    endRound();
 }
 
 function attackHandler() {
@@ -45,3 +66,4 @@ function strongAttackHandler() {
 
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
